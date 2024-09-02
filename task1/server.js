fd3b67cd6bc94e51bf9cffd660bf50dbd3b2d26e
akhilkumar3210 <ak2072719@gmail.com>
2024-09-02 13:34:13 +0530

    Added

--- task1/server.js
+++ task1/server.js
@@ -3,10 +3,8 @@ const fs=require("fs");
 const url=require("url");
 
 http.createServer((req,res)=>{
-    // console.log(req.url);
-    const newUrl=url.parse(req.url)
-    console.log(newUrl);
-    if(newUrl.pathname=="/"){
+    const newurl=url.parse(req.url)
+    if(newurl.pathname=="/"){
         fs.readFile("./pages/index.html",(error,data)=>{
             if(error){
             res.writeHead(404,{"Content-Type":"text/html"})
@@ -18,8 +16,20 @@ http.createServer((req,res)=>{
 
         })
     }
-    else if(newUrl.pathname=="/contact"){
+    else if(newurl.pathname=="/contact"){
         fs.readFile("./pages/contact.html",(error,data)=>{
+            if(error){
+            res.writeHead(404,{"Content-Type":"text/html"})
+            return res.end("Page Not Found")
+            }
+            res.writeHead(200,{"Content-Type":"text/html"})
+            res.write(data);
+            res.end();
+
+        })
+    }
+    else if(newurl.pathname=="/about"){
+        fs.readFile("./pages/about.html",(error,data)=>{
             if(error){
             res.writeHead(404,{"Content-Type":"text/html"})
             return res.end("Page Not Found")
@@ -42,4 +52,6 @@ http.createServer((req,res)=>{
 
         })
     }
+
+
 }).listen(3000)
